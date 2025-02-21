b13478ec1ecb4726a759c3b6c508b9e1b7a5cdb4
Et3rniTy <166850890+anantakhanal465@users.noreply.github.com>
2025-02-21 22:00:11 +0545

    Add files via upload

--- Answer_20.cpp
+++ Answer_20.cpp
@@ -1,19 +1,29 @@
-// Find the sum of all the elaments in an array.
+
+
+// Write a program in C++ to convert a decimal number to a hexadecimal number.
+// Sample Output:
+// Input a decimal number: 43
+// The hexadecimal number is : 2B
 
 #include <iostream>
+#include <cmath>
+using namespace std;
 
-int main() {
-    int arr[] = {10, 20, 30, 40, 50}; 
-    int sum = 0;
-    int n = sizeof(arr) / sizeof(arr[0]); 
-
-    for (int i = 0; i < n; ++i) {
-        sum += arr[i]; 
+int main(){
+    int decimal;
+    cout << "Input a decimal number: ";
+    cin >> decimal;
+    string hexadecimal = "";
+    while(decimal != 0){
+        int remainder = decimal % 16;
+        if(remainder < 10){
+            hexadecimal = to_string(remainder) + hexadecimal;
+        } else {
+            hexadecimal = (char)(remainder + 55) + hexadecimal;
+        }
+        decimal /= 16;
     }
-
-    std::cout << "The sum of the elements in the array is: " << sum << std::endl;
-
+    cout << "The hexadecimal number is: " << hexadecimal << endl;
+    
     return 0;
 }
-
-
--- Answer_6.cpp
+++ Answer_6.cpp
@@ -1,40 +1,23 @@
-// WAP to find the volume of the cube, cuboid and cylinder using function overloading and
-// default arguments.
+// Write a C++ program to compute the sum of the two given integers and count the
+// number of digits in the sum value.
 
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
-// Function to calculate the volume of a cube
-double volume(double side) {
-    return pow(side, 3);
-}
-
-// Function to calculate the volume of a cuboid
-
-double volume(double length, double breadth, double height) {
-    return length * breadth * height;
-}
-
-// Function to calculate the volume of a cylinder
-double volume(double radius, double height) {
-    return M_PI * pow(radius, 2) * height;
-}
-
-int main() {
-    double side, length, breadth, height, radius;
-    cout << "Enter the side of the cube: ";
-    cin >> side;
-    cout << "Volume of the cube is: " << volume(side) << endl;
-
-    cout << "Enter the length, breadth and height of the cuboid: ";
-    cin >> length >> breadth >> height;
-    cout << "Volume of the cuboid is: " << volume(length, breadth, height) << endl;
-
-    cout << "Enter the radius and height of the cylinder: ";
-    cin >> radius >> height;
-    cout << "Volume of the cylinder is: " << volume(radius, height) << endl;
-
+int main()
+{
+    int a, b, sum = 0, digits = 0;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    sum = a + b;
+    int temp = sum;
+    while(temp != 0){
+        temp /= 10;
+        digits++;
+    }
+    cout << "The sum of the two numbers is: " << sum << endl;
+    cout << "The number of digits in the sum is: " << digits << endl;
+    
     return 0;
 }

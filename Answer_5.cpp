2df678d99b94733af55278d8e74b3c782bac6f43
Et3rniTy <166850890+anantakhanal465@users.noreply.github.com>
2025-02-21 22:08:19 +0545

    Add files via upload

--- Answer_5.cpp
+++ Answer_5.cpp
@@ -1,31 +1,37 @@
 
-// Write a C++ program to list non-prime numbers from 1 to an upperbound.
-// Sample Output:
-// Input the upperlimit: 25
-// The non-prime numbers are:
-// 4 6 8 9 10 12 14 15 16 18 20 21 22 24 25
+
+// Write a C++ program to update every array element by multiplication of the next and previous values of a given array of integers.
 
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n;
-    cout << "Input the upperlimit: ";
-    cin >> n;
-    cout << "The non-prime numbers are:" << endl;
-    for(int i = 2; i <= n; i++){
-        bool isPrime = true;
-        for(int j = 2; j * j <= i; j++){
-            if(i % j == 0){
-                isPrime = false;
-                break;
-            }
-        }
-        if(!isPrime){
-            cout << i << " ";
-        }
+void updateArray(int arr[], int n)
+{
+    if (n <= 1)
+        return;
+
+    int prev = arr[0];
+    arr[0] = arr[0] * arr[1];
+
+    for (int i = 1; i < n - 1; i++)
+    {
+        int curr = arr[i];
+        arr[i] = prev * arr[i + 1];
+        prev = curr;
     }
-    cout << endl;
-    
+
+    arr[n - 1] = prev * arr[n - 1];
+}
+
+int main()
+{
+    int arr[] = {5, 6, 7, 8, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    updateArray(arr, n);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
     return 0;
 }
